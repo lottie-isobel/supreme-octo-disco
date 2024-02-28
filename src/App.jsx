f51c9eae9d791af3e62bd8c1634368a5320a4455
lottie-isobel <charlotteeaston@hortmail.co.uk>
2024-02-28 14:50:49 +0000

    create functioning todo list

---
+++ src/App.jsx
@@ -0,0 +1,77 @@
+import { useState } from "react";
+import "./styles.css";
+
+function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos(currentTodos => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title: newItem, completed: false },
+    ]);
+
+    setNewItem("");
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos => {
+      return currentTodos.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos => {
+      return currentTodos.filter(todo => todo.id !== id);
+    });
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input
+            type="text"
+            id="item"
+            value={newItem}
+            onChange={e => setNewItem(e.target.value)}
+          />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">Todo List</h1>
+      <ul classNam="list">
+        {todos.length === 0 && "No todos just yet!"}
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <label>
+              <input
+                type="checkbox"
+                checked={todo.completed}
+                onChange={e => toggleTodo(todo.id, e.target.checked)}
+              />
+              {todo.title}
+            </label>
+            <button
+              onClick={() => deleteTodo(todo.id)}
+              className="btn btn-danger"
+            >
+              Delete
+            </button>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
